107fcc8d73db32db16542227313668c785cfbaae
juarez-gonza <gonza4266118@gmail.com>
2020-08-19 13:40:44 -0300

    super script

---
+++ super_script_que_corre_todos_los_tests_de_la_materia.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+import os
+import re
+
+for root, dirs, files in os.walk("."):
+    for f in files:
+        if re.match("^test.*", f):
+            test_path = os.path.join(root, f)
+            os.system(f"pipenv run python3 {test_path}")
