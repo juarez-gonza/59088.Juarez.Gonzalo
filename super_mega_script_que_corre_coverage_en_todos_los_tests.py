20a3d3ee61f1923dff7d87f94325f58af3db52fe
juarez-gonza <gonza4266118@gmail.com>
2020-08-23 17:11:23 -0300

    quitando pipenv

--- super_mega_script_que_corre_coverage_en_todos_los_tests.py
+++ super_mega_script_que_corre_coverage_en_todos_los_tests.py
@@ -1,4 +1,10 @@
 #! /usr/bin/python3
+
+# Hace lo mismo que su version en shell nada mas
+# que no me había acordado de la existencia del comando
+# find en shell pero sí de os.walk en el momento
+# que se me ocurrio automatizar esto jejox
+
 import os
 import re
 
--- super_mega_script_que_corre_coverage_en_todos_los_tests.sh
+++ super_mega_script_que_corre_coverage_en_todos_los_tests.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
+
+# ughh si solo 1 linea de script mucho mas lindo
+# que su version con os.walk en python
+
 find -iregex ".*/test.*\.py" -exec coverage run --append {} \;
+exit 0
