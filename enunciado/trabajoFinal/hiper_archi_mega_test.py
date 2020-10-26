63c6d75fa4988f3f8cd077df129bcc1d0ee65221
juarez-gonza <gonza4266118@gmail.com>
2020-10-25 22:19:17 -0300

    quitado tp final de este repo

--- enunciado/trabajoFinal/hiper_archi_mega_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import os
-suite = unittest.TestSuite()
-loader = unittest.TestLoader()
-runner = unittest.TextTestRunner()
-tests = loader.discover(os.path.dirname(os.path.abspath(__file__)), "test*.py")
-suite.addTests(tests)
-runner.run(suite)
